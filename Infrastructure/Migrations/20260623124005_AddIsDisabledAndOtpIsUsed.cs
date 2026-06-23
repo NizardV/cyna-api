ecca23b670bb0732159d3871502a1f8f6c87e7d2
Nizard <nizard.verdenal@diiage.org>
2026-06-23 16:05:11 +0200

    fix: migration for user auth problems

--- Infrastructure/Migrations/20260623124005_AddIsDisabledAndOtpIsUsed.cs
+++ Infrastructure/Migrations/20260623124005_AddIsDisabledAndOtpIsUsed.cs
@@ -10,13 +10,31 @@ namespace Infrastructure.Migrations.Postgres
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUsed",
+                table: "PasswordResetCodes",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
 
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUsed",
+                table: "EmailVerificationCodes",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "IsUsed",
+                table: "PasswordResetCodes");
 
+            migrationBuilder.DropColumn(
+                name: "IsUsed",
+                table: "EmailVerificationCodes");
         }
     }
 }
